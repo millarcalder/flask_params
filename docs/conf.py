e19e8c253d70863a0279d8759fa8951ed736b342
millarcalder <millar9819@gmail.com>
2025-03-15 20:21:21 +0000

    formatting

--- docs/conf.py
+++ docs/conf.py
@@ -1,14 +1,12 @@
-project = 'flask_parameters'
-copyright = '2023, Millar Calder'
-author = 'Millar Calder'
-release = '0.0.2'
+project = "flask_parameters"
+copyright = "2023, Millar Calder"
+author = "Millar Calder"
+release = "0.0.2"
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.autosummary']
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary"]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_theme = 'alabaster'
-html_theme_options = {
-    'sidebar_width': '250px'
-}
+html_theme = "alabaster"
+html_theme_options = {"sidebar_width": "250px"}
